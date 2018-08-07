c328021fd6a73198a5eea6fe12e8ce4db072b71f
HswTime <hsw.time@gmail.com>
2018-08-07 17:16:09 +0800

    add text classification

--- Projects/text_classification_cnn_rnn/cnn_model.py
+++ Projects/text_classification_cnn_rnn/cnn_model.py
@@ -9,18 +9,14 @@ import tensorflow as tf
 
 
 class TCNNConfig(object):
-    '''
-    CNN网络配置参数
-    '''
+    """CNN配置参数"""
 
     embedding_dim = 64  # 词向量维度
     seq_length = 600  # 序列长度
     num_classes = 10  # 类别数
-
     num_filters = 256  # 卷积核数目
     kernel_size = 5  # 卷积核尺寸
-
-    vocab_size = 5000  # 词汇表大小
+    vocab_size = 5000  # 词汇表达小
 
     hidden_dim = 128  # 全连接层神经元
 
@@ -28,40 +24,40 @@ class TCNNConfig(object):
     learning_rate = 1e-3  # 学习率
 
     batch_size = 64  # 每批训练大小
-    num_epochs = 10  # 每多少轮存入tensorboard
+    num_epochs = 10  # 总迭代轮次
+
+    print_per_batch = 100  # 每多少轮输出一次结果
+    save_per_batch = 10  # 每多少轮存入tensorboard
 
 
 class TextCNN(object):
-    '''
-    文本分类,CNN模型
-    '''
+    """文本分类，CNN模型"""
 
     def __init__(self, config):
         self.config = config
 
         # 三个待输入的数据
-        self.input_x = tf.placeholder(tf.int32, shape=[None, self.config.seq_length], name='input_x')
-        self.input_y = tf.placeholder(tf.int32, shape=[None, self.config.num_classes], name='input_y')
+        self.input_x = tf.placeholder(tf.int32, [None, self.config.seq_length], name='input_x')
+        self.input_y = tf.placeholder(tf.float32, [None, self.config.num_classes], name='input_y')
         self.keep_prob = tf.placeholder(tf.float32, name='keep_prob')
 
         self.cnn()
 
     def cnn(self):
-        '''
-        CNN模型
-        :return:
-        '''
+        """CNN模型"""
         # 词向量映射
-        # 构建特征,一共5000个词,每个词用64维表示
         with tf.device('/cpu:0'):
-            embedding = tf.get_variable(name='embedding', shape=[self.config.vocab_size, self.config.embedding_dim])
+            embedding = tf.get_variable('embedding', [self.config.vocab_size, self.config.embedding_dim])
             embedding_inputs = tf.nn.embedding_lookup(embedding, self.input_x)
 
-        with tf.name_scope('cnn'):
+        with tf.name_scope("cnn"):
+            # CNN layer
             conv = tf.layers.conv1d(embedding_inputs, self.config.num_filters, self.config.kernel_size, name='conv')
+            # global max pooling layer
             gmp = tf.reduce_max(conv, reduction_indices=[1], name='gmp')
-        with tf.name_scope('score'):
-            # 全连接层,后面接dropout以及relu激活
+
+        with tf.name_scope("score"):
+            # 全连接层，后面接dropout以及relu激活
             fc = tf.layers.dense(gmp, self.config.hidden_dim, name='fc1')
             fc = tf.contrib.layers.dropout(fc, self.keep_prob)
             fc = tf.nn.relu(fc)
@@ -70,13 +66,14 @@ class TextCNN(object):
             self.logits = tf.layers.dense(fc, self.config.num_classes, name='fc2')
             self.y_pred_cls = tf.argmax(tf.nn.softmax(self.logits), 1)  # 预测类别
 
-        with tf.name_scope('optimize'):
-            # 损失函数,交叉熵
+        with tf.name_scope("optimize"):
+            # 损失函数，交叉熵
             cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=self.logits, labels=self.input_y)
             self.loss = tf.reduce_mean(cross_entropy)
             # 优化器
             self.optim = tf.train.AdamOptimizer(learning_rate=self.config.learning_rate).minimize(self.loss)
 
-        with tf.name_scope('accuracy'):
+        with tf.name_scope("accuracy"):
+            # 准确率
             correct_pred = tf.equal(tf.argmax(self.input_y, 1), self.y_pred_cls)
             self.acc = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
--- Projects/text_classification_cnn_rnn/helper/cnews_loader.py
+++ Projects/text_classification_cnn_rnn/helper/cnews_loader.py
@@ -11,21 +11,16 @@ import tensorflow.contrib.keras as kr
 
 
 def open_file(filename, mode='r'):
-    '''
-    打开文件,返回文件句柄
-    :param filename:
-    :param mode:
-    :return:
-    '''
+    """
+    常用文件操作
+    mode: 'r' or 'w' for read or write
+    """
+
     return open(filename, mode, encoding='utf-8', errors='ignore')
 
 
 def read_file(filename):
-    '''
-    读取文件数据
-    :param filename:
-    :return:
-    '''
+    """读取文件数据"""
     contents, labels = [], []
     with open_file(filename) as f:
         for line in f:
@@ -40,69 +35,53 @@ def read_file(filename):
 
 
 def build_vocab(train_dir, vocab_dir, vocab_size=5000):
-    '''
-    根据训练集构建词汇表,然后存储到文件中
-    :param train_dir:
-    :param vocab_dir:
-    :param vocab_size:
-    :return:
-    '''
+    """根据训练集构建词汇表，存储"""
     data_train, _ = read_file(train_dir)
-    # print(len(data_train)) 50000
 
-    # 将50000条数据中的单词进行收集
     all_data = []
     for content in data_train:
         all_data.extend(content)
 
     counter = Counter(all_data)
-    # most_common对单词进行计算,只要最多的指定个数的单词
     count_pairs = counter.most_common(vocab_size - 1)
-    # 写法不错.取出单词,不要统计的个数
     words, _ = list(zip(*count_pairs))
-
-    # 在头部拼接上<PAD>
     # 添加一个 <PAD> 来将所有文本pad为同一长度
     words = ['<PAD>'] + list(words)
-    print(len(words))
     open_file(vocab_dir, mode='w').write('\n'.join(words) + '\n')
 
 
 def read_vocab(vocab_dir):
-    '''
-    读取词汇表,将词汇数值化
-    :param vocab_dir:
-    :return:
-    '''
-    with open_file(vocab_dir) as f:
-        words = [_.strip() for _ in f.readlines()]
-    words_to_id = dict(zip(words, range(len(words))))
-    return words, words_to_id
+    """读取词汇表"""
+    # words = open_file(vocab_dir).read().strip().split('\n')
+    with open_file(vocab_dir) as fp:
+        words = [_.strip() for _ in fp.readlines()]
+    word_to_id = dict(zip(words, range(len(words))))
+    return words, word_to_id
 
 
 def read_category():
-    '''
-    读取分类目录
-    :return:
-    '''
+    """读取分类目录，固定"""
     categories = ['体育', '财经', '房产', '家居', '教育', '科技', '时尚', '时政', '游戏', '娱乐']
+
+    categories = [x for x in categories]
+
     cat_to_id = dict(zip(categories, range(len(categories))))
+
     return categories, cat_to_id
 
 
+def to_words(content, words):
+    """将id表示的内容转换为文字"""
+    return ''.join(words[x] for x in content)
+
+
 def process_file(filename, word_to_id, cat_to_id, max_length=600):
-    '''
-    将文件转换为id 表示
-    :param filename:
-    :param word_to_id:
-    :param cat_to_id:
-    :param max_length:
-    :return:
-    '''
+    """将文件转换为id表示"""
     contents, labels = read_file(filename)
+
     data_id, label_id = [], []
     for i in range(len(contents)):
-        data_id.append(word_to_id[x] for x in contents[i] if x in word_to_id)
+        data_id.append([word_to_id[x] for x in contents[i] if x in word_to_id])
         label_id.append(cat_to_id[labels[i]])
 
     # 使用keras提供的pad_sequences来将文本pad为固定长度
@@ -113,11 +92,15 @@ def process_file(filename, word_to_id, cat_to_id, max_length=600):
 
 
 def batch_iter(x, y, batch_size=64):
-    '''
-    生成批次数据
-    :param x:
-    :param y:
-    :param batch_size:
-    :return:
-    '''
-    pass
+    """生成批次数据"""
+    data_len = len(x)
+    num_batch = int((data_len - 1) / batch_size) + 1
+
+    indices = np.random.permutation(np.arange(data_len))
+    x_shuffle = x[indices]
+    y_shuffle = y[indices]
+
+    for i in range(num_batch):
+        start_id = i * batch_size
+        end_id = min((i + 1) * batch_size, data_len)
+        yield x_shuffle[start_id:end_id], y_shuffle[start_id:end_id]
